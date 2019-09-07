7bb55841f37fd28d3c0a806879de373e4b368929
JSherlcok <1114932426@qq.com>
2019-09-07 18:05:36 +0800

    完成了登录、授权以及用户信息的初始化及其缓存

--- src/test/java/com/slxy/analysis/AnalysisApplicationTests.java
+++ src/test/java/com/slxy/analysis/AnalysisApplicationTests.java
@@ -4,13 +4,18 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import redis.clients.jedis.Jedis;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class AnalysisApplicationTests {
 
+
 	@Test
 	public void contextLoads() {
+		Jedis jedis = new Jedis("43.235.43.59",6379);
+		System.out.println(jedis.ping());
+		System.out.println(jedis.get("Back1"));
 	}
 
 }
