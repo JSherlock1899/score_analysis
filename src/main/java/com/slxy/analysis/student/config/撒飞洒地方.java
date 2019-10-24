542000da367072cf227f16374163ed15f46eeb5c
JSherlcok <1114932426@qq.com>
2019-10-24 16:21:17 +0800

    添加了部分学生端的内容

--- src/main/java/com/slxy/analysis/student/config/撒飞洒地方.java
+++
@@ -1,5 +0,0 @@
-package com.slxy.analysis.student.config;
-
-public class 撒飞洒地方 {
-
-}
--- src/test/java/com/slxy/analysis/AnalysisApplicationTests.java
+++ src/test/java/com/slxy/analysis/AnalysisApplicationTests.java
@@ -1,5 +1,6 @@
 package com.slxy.analysis;
 
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
@@ -9,11 +10,9 @@ import org.springframework.test.context.junit4.SpringRunner;
 public class AnalysisApplicationTests {
 
 
-//	@Test
-//	public void contextLoads() {
-//		Jedis jedis = new Jedis("43.235.43.59",6379);
-//		System.out.println(jedis.ping());
-//		System.out.println(jedis.get());
-//	}
+	@Test
+	public void contextLoads() {
+
+	}
 
 }
